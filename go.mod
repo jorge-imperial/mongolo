715c4ba646fe70ad3c5509a08131cfc09c7fef58
Jorge Imperial <jorgeluis.imperial@gmail.com>
2022-03-12 10:14:06 -0800

    Using command line parameters

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module mongolo
 
 go 1.16
 
-require github.com/mongodb/ftdc v0.0.0-20220201142920-3ba24b40c7da
+require (
+	fyne.io/fyne/v2 v2.1.3 // indirect
+	github.com/mongodb/ftdc v0.0.0-20220201142920-3ba24b40c7da
+	go.mongodb.org/mongo-driver v1.8.4 // indirect
+)
--- mongolo.go
+++ mongolo.go
@@ -3,16 +3,23 @@ package main
 import (
 	"bytes"
 	"context"
+	"flag"
 	"fmt"
 	"github.com/mongodb/ftdc"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"io/ioutil"
+	"log"
 	"os"
-	"strconv"
+	"strings"
+	"time"
 )
 
-func mongolo(path string, chunkCount int) {
+func dumpMetricsToScreen(path string, start int, end int) {
 	file, _ := os.Open(path)
 	data, _ := ioutil.ReadAll(file)
+	file.Close()
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -22,16 +29,112 @@ func mongolo(path string, chunkCount int) {
 	for iter.Next() {
 		c := iter.Chunk()
 
-		// Output all metrics
-		for i, metric := range c.Metrics {
-			fmt.Print(i, ";", metric.Key(), ";", metric.Values[0])
-			for j := 1; j < len(metric.Values); j++ {
-				fmt.Print(";", metric.Values[j])
-			}
-			fmt.Println("")
-		}
 		counter++
-		if counter == chunkCount {
+		if counter >= start {
+			// Output all metrics
+			for i, metric := range c.Metrics {
+				fmt.Print(i, ";", metric.Key(), ";", metric.Values[0])
+				for j := 1; j < len(metric.Values); j++ {
+					fmt.Print(";", metric.Values[j])
+				}
+				fmt.Println("")
+			}
+		}
+
+		if counter == end {
+			break
+		}
+	}
+}
+
+func dumpNamesToScreen(fileName string, start int, end int) {
+	ctx, _ := context.WithCancel(context.Background())
+	f, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Error reading file.")
+		panic(err)
+	}
+	data, err := ioutil.ReadAll(f)
+
+	var iter = ftdc.ReadMetrics(ctx, bytes.NewBuffer(data))
+	counter := 0
+	for iter.Next() {
+		counter++
+		doc := iter.Document()
+		metricsMap := doc.ExportMap()
+
+		if counter >= start {
+			i := 0
+			for k, v := range metricsMap {
+				fmt.Println("# ", i, k, "=", v)
+				i++
+			}
+			fmt.Println("# ----------------------------------------------------------------------")
+		}
+
+		if counter == end {
+			break
+		}
+	}
+	f.Close()
+}
+
+func dumpMetricsToCollections(fileName string, uri string, start int, end int) {
+
+	// "mongodb+srv://<username>:<password>@lighthouse.rmm5w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
+	clientOptions := options.Client().
+		ApplyURI(uri).
+		SetServerAPIOptions(serverAPIOptions)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	database := client.Database(strings.Replace(fileName, ".", "_", -1))
+
+	// Read all in.
+	file, _ := os.Open(fileName)
+	data, _ := ioutil.ReadAll(file)
+	file.Close()
+	defer cancel()
+
+	iter := ftdc.ReadChunks(ctx, bytes.NewBuffer(data))
+	counter := 0
+
+	for iter.Next() {
+		c := iter.Chunk()
+		m := c.GetMetadata().ExportMap()
+		fmt.Println(m["_id"])
+		doc := m["doc"]
+
+		fmt.Println(doc)
+		counter++
+		if counter >= start {
+			// First metric is timestamp
+			ts := c.Metrics[0].Values
+			for i, metric := range c.Metrics {
+				fmt.Println(i, metric.Key())
+
+				for j := 0; j < len(metric.Values); j++ {
+					//
+					collName := strings.Replace(metric.Key(), ".", "_", -1)
+					coll := database.Collection(collName)
+					doc := bson.D{{"ts", ts[j]}, {"value", metric.Values[j]}}
+					fmt.Println(coll, doc)
+					_, err := coll.InsertOne(context.TODO(), doc)
+					if err != nil {
+						panic(err)
+					}
+
+				}
+			}
+		}
+
+		if counter == end {
 			break
 		}
 	}
@@ -40,44 +143,39 @@ func mongolo(path string, chunkCount int) {
 
 func main() {
 
-	args := os.Args[1:]
+	// Command line variables
+	var startChunk int
+	var endChunk int
+	var dumpNames bool
+	var dumpMetrics bool
+	var dumpMongo bool
+	var fileName string
+	var connectionString string
+	flag.IntVar(&startChunk, "start", 0, "Starting chunk to dump (zero is first chunk).")
+	flag.IntVar(&endChunk, "end", 0, "Last chunk to dump.")
+	flag.BoolVar(&dumpNames, "names", true, "Output metric names contents to stdout.")
+	flag.BoolVar(&dumpMetrics, "metrics", false, "Output metric values to stdout.")
+	flag.BoolVar(&dumpMongo, "mongo", false, "Create collections using metrics.")
 
-	ctx, _ := context.WithCancel(context.Background())
-	fmt.Println("# --------------------- Reading ", args[1], " chunks from ", args[0])
+	flag.StringVar(&fileName, "file", "", "Path to file to read.")
+	flag.StringVar(&connectionString, "uri", "mongodb://localhost:27017/", "Connection string to MongoDB database")
 
-	f, err := os.Open(args[0])
-	if err != nil {
-		fmt.Println("Error reading file.")
-		panic(err)
-	}
-	data, err := ioutil.ReadAll(f)
+	flag.Parse()
 
-	numDocs, err := strconv.Atoi(args[1])
-	if err != nil {
-		fmt.Println("Second parameter should be an integer")
-		panic(err)
-	}
-
-	var iter = ftdc.ReadMetrics(ctx, bytes.NewBuffer(data))
-	counter := 0
-	for iter.Next() {
-
-		counter++
-
-		doc := iter.Document()
-		metricsMap := doc.ExportMap()
-
-		fmt.Println("# ----------------------------------------------------------------------")
-		i := 0
-		for k, v := range metricsMap {
-			fmt.Println("# ", i, k, "=", v)
-			i++
-		}
-		if counter == numDocs {
-			break
-		}
+	// make sure we have sane start and stop chunk values
+	if endChunk < startChunk {
+		endChunk = startChunk
 	}
 
-	numDocs, _ = strconv.Atoi(args[1])
-	mongolo(args[0], numDocs)
+	fmt.Println("# Reading ", fileName, " chunks from ", startChunk, " to ", endChunk)
+
+	if dumpNames {
+		dumpNamesToScreen(fileName, startChunk, endChunk)
+	}
+	if dumpMetrics {
+		dumpMetricsToScreen(fileName, startChunk, endChunk)
+	}
+	if dumpMongo {
+		dumpMetricsToCollections(fileName, connectionString, startChunk, endChunk)
+	}
 }
